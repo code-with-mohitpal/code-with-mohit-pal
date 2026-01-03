104f4d1103121c71406d129adb35fad688c18237
Mohit pal <pal001768@gmail.com>
2026-01-03 23:23:23 +0530

    53. Maximum Subarray

---
+++ 53. Maximum Subarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum=nums[0];
+        int currentSum=0;
+        for(int x:nums){
+            currentSum +=x;
+            if(currentSum>maxSum)
+              maxSum=currentSum;
+            if(currentSum<0)
+              currentSum=0;
+
+        }
+        return maxSum;
+    }
+}
